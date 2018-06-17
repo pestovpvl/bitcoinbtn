70b093502eb8ef50b5e5aed8708bab70b3bc472e
pestovpvl <pestovpvl@mail.ru>
2018-06-17 10:19:17 -0400

    Fixed input value to correct format

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,5 +1,10 @@
 class Coin < ApplicationRecord
     belongs_to :person
+    before_validation :make_only_symbol_for_coin
+  private
+    def make_only_symbol_for_coin
+      self.symbol = symbol.split.first
+    end
 end
 
 
