7c3d18f2064ca3d2a8c03a5465e999fe03e03bb6
pestovpvl <pestovpvl@mail.ru>
2018-06-23 20:41:29 -0400

    Finish history page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
 class StaticPagesController < ApplicationController
   def about
   end
+  def history
+    @coins = Coin.all
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/?limit=1000'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coins = JSON.parse(@response)
+    @profit_lost = 0
+    @profit_summ = 0
+  end
 end
